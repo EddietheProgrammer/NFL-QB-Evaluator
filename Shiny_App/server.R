08bd38bfd08e00a34ad40da32e96d282354499b2
Calvinzheng123 <calvi@MacBook-Air-54.local>
2023-10-20 21:43:01 -0400

    Shiny Regression Model

--- Shiny_App/server.R
+++ Shiny_App/server.R
@@ -1,28 +1,43 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-
-# Define server logic required to draw a histogram
-function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
-}
+shinyServer(function(input, output) {
+  
+  selectedData <- reactive({
+    USArrests[, c("UrbanPop", input$ycol)]
+  })
+  
+  model1 <- lm(Murder ~ UrbanPop, data = USArrests)
+  model2 <- lm(Assault ~ UrbanPop, data = USArrests)
+  model3 <- lm(Rape ~ UrbanPop, data = USArrests)
+  
+  output$plot1 <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData())
+    
+    if(input$ycol=='Murder'){abline(model1, col = "blue", lwd = 2)}
+    if(input$ycol=='Assault'){abline(model2, col = "green", lwd = 2)}
+    if(input$ycol=='Rape'){abline(model3, col = "red", lwd = 2)}
+    
+  })
+  
+  output$pred1p <- renderText({if(input$ycol=='Murder'){anova(model1)$'Pr(>F)'[1]}})
+  output$pred2p <- renderText({if(input$ycol=='Assault'){anova(model2)$'Pr(>F)'[1]}})
+  output$pred3p <- renderText({if(input$ycol=='Rape'){anova(model3)$'Pr(>F)'[1]}})
+  
+  output$pred1slope <- renderText({if(input$ycol=='Murder'){model1[[1]][2]}})
+  output$pred2slope <- renderText({if(input$ycol=='Assault'){model2[[1]][2]}})
+  output$pred3slope <- renderText({if(input$ycol=='Rape'){model3[[1]][2]}})
+  
+  output$pred1intercept <- renderText({if(input$ycol=='Murder'){model1[[1]][1]}})
+  output$pred2intercept <- renderText({if(input$ycol=='Assault'){model2[[1]][1]}})
+  output$pred3intercept <- renderText({if(input$ycol=='Rape'){model3[[1]][1]}})
+  
+  output$table <- renderTable({
+    head(selectedData(),10)
+  })
+  
+  
+  
+  
+  
+  
+})
--- Shiny_App/ui.R
+++ Shiny_App/ui.R
@@ -1,33 +1,33 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-
-# Define UI for application that draws a histogram
-fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+shinyUI(fluidPage(
+  titlePanel('Violent Crime Rates vs Urban Population in the US'),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput('ycol', 'Select a type of Crime to visualize its relation to percentage of urban population', names(USArrests)[c(1,2,4)],
+                  selected=names(USArrests)[[1]]),
+      h1(""),
+      h5("head of dataset"),      
+      tableOutput('table')      
+    ),
+    mainPanel(
+      h5("The chart shows amount of crimes (in arrests per 100,000 residents) vs the percent of the population living in urban areas in each of the 50 US states in 1973."),
+      h5("Source: 'USArrests' dataset from R."),
+      h5("A linear regression model is calculated for each type of crime and shown below."),
+      plotOutput("plot1"),
+      h4("Slope"),
+      textOutput("pred1slope"),
+      textOutput("pred2slope"),
+      textOutput("pred3slope"),
+      h4("Intercept"),
+      textOutput("pred1intercept"),
+      textOutput("pred2intercept"),
+      textOutput("pred3intercept"),
+      h5("The linear model of crime explained by percent of urban population can be evaluated with the P value"),
+      h4("P Value of Regression Model"),
+      textOutput("pred1p"),
+      textOutput("pred2p"),
+      textOutput("pred3p")
+      
     )
-)
+  )
+))
